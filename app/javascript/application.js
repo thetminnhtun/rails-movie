74742964281b745f3548ef59af47b6564def0215
Thet Minn Htun <59432845+thetminnhtun-scm@users.noreply.github.com>
2023-12-21 15:46:45 +0630

    Customize navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", function() {
+    const dropdownButton = document.getElementById('dropdownNavbarLink');
+    const dropdownMenu = document.getElementById('dropdownNavbar');
+
+    dropdownButton?.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('hidden');
+    });
+});
